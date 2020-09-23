32ff83ca11edda24c3f1e8d17c90e7611b052341
emuzhani <emuzhani@eagleprojects.it>
2020-09-23 13:55:45 +0200

    def

---
+++ hello.js
@@ -0,0 +1,4 @@
+var hello = "Hello, World!";
+var integerNumber = 1;
+var doublenumber = 1.234;
+console.log("STAMPA" + hello + " " + integerNumber + "\n");
---
+++ hello.ts
@@ -0,0 +1,8 @@
+let hello:string = "Hello, World!";
+
+
+let integerNumber:number = 1;
+let doublenumber:number = 1.234;
+
+
+console.log("STAMPA " + hello + " " + integerNumber + "\n");
