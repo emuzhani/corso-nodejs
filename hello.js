629fbe55192796af6f4aaa9e9043fea47ba88596
emuzhani <emuzhani@eagleprojects.it>
2020-09-30 13:53:09 +0200

    Lezione

--- hello.js
+++ hello.js
@@ -1,4 +1,55 @@
 var hello = "Hello, World!";
 var integerNumber = 1;
 var doublenumber = 1.234;
-console.log("STAMPA" + hello + " " + integerNumber + "\n");
+console.log("STAMPA " + hello + " " + integerNumber + "\n");
+var miaTupla = [1, "pippo"]; //UNA TUPLA HA SOLO 2 ELEMENTI, DI QUESTO TIPO ED IN QUESTO ORDINE
+console.log(miaTupla);
+//ENUM, IL NOME DEVE SEMPRE INIZIARE CON LA MAIUSCOLA 
+var Colori;
+(function (Colori) {
+    Colori["red"] = "red";
+    Colori["green"] = "green";
+    Colori["blue"] = "blue";
+})(Colori || (Colori = {}));
+;
+var color1 = Colori.blue;
+console.log(color1);
+var anyValue; //ANY VALUE TYPE AVAIABLE; CAN BE ASSIGNED TO ANOTHER VAR
+anyValue = "papp";
+var unknownValue; //ANY VALUE TYPE AVAIABLE; CANNOT BE ASSIGNED TO ANOTHER VAR
+unknownValue = false;
+/*
+const segno = function(){
+    while(true)
+    {
+        console.log("pippo");
+        console.log("pluto");
+        console.log("paperino");
+
+    }
+}
+*/
+var failError = function (message) {
+    throw new Error(message);
+};
+var object = {
+    nome: "Ciao",
+    cognome: "Moltociao",
+    eta: 12
+};
+console.log(object.cognome);
+for (var i = 0; i < 5; i++) {
+    console.log(i * 4);
+}
+var miaEta = 59;
+switch (miaEta) {
+    case 23:
+        console.log("wawa");
+        break;
+    case 59:
+        console.log("Behottimo");
+        break;
+    default:
+        console.log("Ah");
+        break;
+}
--- hello.ts
+++ hello.ts
@@ -6,3 +6,64 @@ let doublenumber:number = 1.234;
 
 
 console.log("STAMPA " + hello + " " + integerNumber + "\n");
+
+let miaTupla: [number, string] = [1, "pippo"]; //UNA TUPLA HA SOLO 2 ELEMENTI, DI QUESTO TIPO ED IN QUESTO ORDINE
+console.log(miaTupla);
+
+
+//ENUM, IL NOME DEVE SEMPRE INIZIARE CON LA MAIUSCOLA 
+enum Colori{red ="red", green ="green", blue ="blue"};
+let color1: Colori = Colori.blue;
+console.log(color1);
+
+let anyValue: any; //ANY VALUE TYPE AVAIABLE; CAN BE ASSIGNED TO ANOTHER VAR
+anyValue = "papp";
+
+let unknownValue: unknown; //ANY VALUE TYPE AVAIABLE; CANNOT BE ASSIGNED TO ANOTHER VAR
+unknownValue = false;
+
+/*
+const segno = function(){
+    while(true)
+    {
+        console.log("pippo");
+        console.log("pluto");
+        console.log("paperino");
+
+    }
+}
+*/
+
+const failError = function(message:string){
+    throw new Error(message)
+}
+
+
+let object = {
+    nome: "Ciao",
+    cognome: "Moltociao",
+    eta: 12
+};
+
+console.log(object.cognome);
+
+for(let i=0; i<5; i++)
+{
+    console.log(i*4);
+}
+
+
+let miaEta = 59;
+
+switch (miaEta)
+{
+    case 23: console.log("wawa");
+    break;
+
+    case 59: console.log("Behottimo");
+    break;
+
+    default: console.log("Ah");
+    break;
+}
+
